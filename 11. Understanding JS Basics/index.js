94d0c9882b506b61926befe840a268569bebcc9a
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-12 09:19:40 +0200

    started js basics

---
+++ 11. Understanding JS Basics/index.js
@@ -0,0 +1,11 @@
+let age = 32;
+let userName = "Ciprian";
+let hobbies = ["sports", "cooking", "reading"]; // arrays
+let job = {
+  companyName: "Company",
+  team: "NIS",
+  title: "Developer",
+  place: "IS-RO",
+}; // objects
+
+// GOAL - code readability
