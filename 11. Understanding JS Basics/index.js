c61f6609aa0f0b402c26021619e3e18e2863b092
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-13 11:19:34 +0200

    added section 11

--- 11. Understanding JS Basics/index.js
+++ 11. Understanding JS Basics/index.js
@@ -9,3 +9,39 @@ let job = {
 }; // objects
 
 // GOAL - code readability
+
+// take an element based on its idx
+const goals = ["learn web development", "learn algo", "do different chores"];
+const getListItem = (arr, arrayIdx) => {
+  let arrayElement = arr[arrayIdx];
+  return arrayElement;
+};
+
+let myfirstGoal = getListItem(goals, 0);
+
+// Math operations
+// console.log(10 + 4);
+// console.log(10 - 4);
+// console.log(10 * 4);
+// console.log(10 / 4);
+// console.log(10 % 4);
+
+let result = 10 + 3 - 5 * 10;
+result = 10 * 4;
+
+result++; // result = result + 1
+result--; // result = result - 1
+
+result += 5; // result =  result + 5
+result -= 5; // result = result - 5
+result /= 5; // result = result / 5
+result *= 5; // result = result * 5
+
+// String Operations
+let username = "Ciprian";
+console.log(username.length);
+console.log(username.toUpperCase());
+
+// Basic Array Operations
+let myHobbies = ["Sports", "Cooking", "Coding"];
+console.log(myHobbies.length);
